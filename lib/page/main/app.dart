579b90275198e8d157ce2007680f75d053bc4479
Wagner Soares <wagnersoares.redgaw@gmail.com>
2025-08-17 21:59:51 -0300

    iniciando repositorio

---
+++ lib/page/main/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:lista_tarefas/page/home/home_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/page/main/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:lista_tarefas/page/main/app.dart';
+
+void main() {
+  runApp(const App());
+}
